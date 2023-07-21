8dbecfd0a175e3a75bb60b32d781c16a58e4fb78
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2023-07-21 14:31:00 -0400

    add constuctor for books and function that adds books based on form values + adds book to library array

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
 const newBookModal = document.getElementById(`new-book-modal`)
+const library = []
+const submitButton = document.getElementById(`submit-book`)
+
+const newBookTitle = document.getElementById(`title`)
+const newBookAuthor = document.getElementById(`author`)
+const newBookPages = document.getElementById(`pages`)
+const newBookRead = document.getElementById(`read-status`)
 
 function toggleNewBookModal() {
     if (newBookModal.style.display==="flex") {
@@ -6,3 +13,17 @@ function toggleNewBookModal() {
         } else {newBookModal.style.display="flex"
     }
 }
+
+function Book(title,author,pages,read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+function addBook() {
+    const newBook = new Book(`${newBookTitle.value}`,`${newBookAuthor.value}`,`${newBookPages.value}`,`${newBookRead.value}`)
+    library.push(newBook)
+}
+
+
