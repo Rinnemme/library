0e13e1734e55f378b7c717f2343cd822661f398a
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2023-07-21 15:54:09 -0400

    add function to book-remove buttons that will remove associated element from dom AND object associated with that element from the library array; make submit button function add an element to the dom with the book's title and a book-remove button

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const newBookModal = document.getElementById(`new-book-modal`)
-const library = []
+let library = []
 const submitButton = document.getElementById(`submit-book`)
 const libraryGrid = document.getElementById(`library-grid`)
 
@@ -27,15 +27,33 @@ function addBook() {
     library.push(newBook)
 }
 
-function submitBookForm(event) {
-    const newBook = new Book(`${newBookTitle.value}`,`${newBookAuthor.value}`,`${newBookPages.value}`,`${newBookRead.value}`)
-    library.push(newBook)
-    event.preventDefault()
-    toggleNewBookModal()
+function clearForm() {
     newBookTitle.value=""
     newBookAuthor.value=""
     newBookPages.value=""
     newBookRead.value="not read"
+}
+
+function submitBookForm(event) {
+    event.preventDefault()
+    const newBook = new Book(`${newBookTitle.value}`,`${newBookAuthor.value}`,`${newBookPages.value}`,`${newBookRead.value}`)
+    library.push(newBook)
+    const newBookElement = document.createElement("div")
+    newBookElement.classList.add("book")
+    newBookElement.textContent=`${newBook.title}`
+    newBookElement.title=`${newBook.title}`
+    const newRemoveButton = document.createElement("button")
+    newRemoveButton.textContent = "x"
+    newRemoveButton.classList.add("book-remove")
+    newRemoveButton.onclick = function () {removeBook(this.parentElement)}
+    newBookElement.appendChild(newRemoveButton)
+    libraryGrid.appendChild(newBookElement)
+    toggleNewBookModal()
+    clearForm()
+}
+function removeBook(element) {
+    library = library.filter (book => book.title !== `${element.title}`)
+    element.parentElement.removeChild(element)
 }
 
 submitButton.addEventListener("click", submitBookForm)
