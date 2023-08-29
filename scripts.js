ff3b3b45ba80db08475d1dcab06e79e5d4f19826
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2023-08-29 14:56:02 -0400

    add arrays of colors for consistent style across books, make a newBook module to house all functions relating to building elements and adding to the library array

--- scripts.js
+++ scripts.js
@@ -14,6 +14,40 @@ const infoAuthor = document.getElementById(`book-info-author`)
 const infoPages = document.getElementById(`book-info-pages`)
 const infoRead = document.getElementById(`book-info-read`)
 
+const lightColors = [
+    '#FFA9A9', 
+    '#FFC0A9', 
+    '#FFDDA9', 
+    '#CAFFA9', 
+    '#A9FFBC', 
+    '#A9FFE5', 
+    '#A9F2FF', 
+    '#A9D3FF', 
+    '#A9AFFF', 
+    '#B8A9FF', 
+    '#D9A9FF', 
+    '#F6A9FF', 
+    '#FFA9E7', 
+    '#FFA9CD'
+]
+
+const darkColors = [
+    '#862525', 
+    '#8B3F23', 
+    '#8B6120', 
+    '#478421', 
+    '#1D7D32', 
+    '#1F8969', 
+    '#1D7382', 
+    '#235589', 
+    '#202784', 
+    '#34228B', 
+    '#581F84', 
+    '#7C2186', 
+    '#8B1D6C', 
+    '#891F4B'
+]
+
 class Book {
     constructor (title,author,pages,read) {
         this.title = title
@@ -21,61 +55,89 @@ class Book {
         this.pages = pages
         this.read = read
         this.toggleRead = function() {
-            if (this.read === "read") {
-                this.read = "not read"
-            } else this.read = "read"
+            this.read = this.read === 'read' ? 'unread' : 'read'
         }
     }
 }
 
-function addBook() {
-    const book = new Book(`${formTitle.value}`,`${formAuthor.value}`,`${formPages.value}`,`${formRead.value}`)
-    library.push(book)
-}
-
 function toggleNewBookModal() {
     clearForm()
-    newBookModal.style.display = newBookModal.style.display === "flex" ? "none" : "flex"
+    newBookModal.style.display = newBookModal.style.display === 'flex' ? 'none' : 'flex'
 }
 
-function generateBookElement() {
-    const bookElement = document.createElement("div")
-    bookElement.classList.add("book")
-    bookElement.textContent = `${formTitle.value}`
-    bookElement.title = `${formTitle.value}`
-    bookElement.style.backgroundColor = `rgb(${Math.ceil(Math.random()*100)},${Math.ceil(Math.random()*100)},${Math.ceil(Math.random()*100)})`
-    const bookAuthor = document.createElement("p")
-    bookAuthor.textContent = `by ${formAuthor.value}`
-    bookElement.appendChild(bookAuthor)
-    const removeButton = document.createElement("button")
-    removeButton.textContent = "×"
-    removeButton.classList.add("book-remove")
-    removeButton.onclick = function () {removeBook(this.parentElement)}
-    bookElement.appendChild(removeButton)
-    const readButton = document.createElement("button")
-    readButton.textContent = `${formRead.value}`
-    readButton.classList.add("toggle-read")
-    readButton.onclick = function () {toggleBookRead(this)}
-    bookElement.appendChild(readButton)
-    const infoButton = document.createElement("button")
-    infoButton.textContent = "ⓘ"
-    infoButton.classList.add("book-info")
-    infoButton.onclick = function () {displayBookInfo(this)}
-    bookElement.appendChild(infoButton)
-    libraryGrid.appendChild(bookElement)
-}
+const newBook = (() => {
+    const book = new Book(`${formTitle.value}`,`${formAuthor.value}`,`${formPages.value}`,`${formRead.value}`)
+    const colorIndex = Math.floor(Math.random()*lightColors.length)
+    const lightColor = lightColors[colorIndex]
+    const darkColor = darkColors[colorIndex]
+
+    const generate = (() => {
+        const bookElement = document.createElement('div')
+        bookElement.classList.add('book')
+        bookElement.textContent = `${book.title}`
+        bookElement.title = book.title
+        bookElement.style.backgroundColor = `${lightColor}`
+        bookElement.style.color = `${darkColor}`
+        const bookAuthor = document.createElement('p')
+        bookAuthor.textContent = `by ${book.author}`
+        bookElement.appendChild(bookAuthor)
+        bookElement.appendChild(info().button)
+        bookElement.appendChild(read().button)
+        bookElement.appendChild(remove().button)
+        return {bookElement}
+    })
+
+    const info = (() => {
+        const button = document.createElement('button')
+        button.textContent = 'ⓘ'
+        button.style.border = `1px solid ${darkColor}`
+        button.style.backgroundColor = `${lightColor}`
+        button.style.color = `${darkColor}`
+        button.classList.add('book-info')
+        button.onclick = function () {displayBookInfo(this)}
+        return {button}
+    })
+
+    const read = (() => {
+        const button = document.createElement('button')
+        button.textContent = `${book.read}`
+        button.classList.add('toggle-read')
+        button.style.backgroundColor = `${darkColor}`
+        button.style.color = `${lightColor}`
+        button.onclick = function () {toggleBookRead(this)}
+        return {button}
+    })
+
+    const remove = (() => {
+        const button = document.createElement('button')
+        button.textContent = '×'
+        button.style.border = `1px solid ${darkColor}`
+        button.style.backgroundColor = `${lightColor}`
+        button.style.color = `${darkColor}`
+        button.classList.add('book-remove')
+        button.setAttribute('title',`Remove ${book.title}`)
+        button.onclick = function () {removeBook(this.parentElement)}
+        return {button}
+    })
+
+    const addFromForm = () => {
+        library.push(book)
+        libraryGrid.appendChild(generate().bookElement)
+    }
+
+    return {addFromForm}
+}) 
 
 function clearForm() {
-    formTitle.value = ""
-    formAuthor.value = ""
-    formPages.value = ""
-    formRead.value = "not read"
+    formTitle.value = ''
+    formAuthor.value = ''
+    formPages.value = ''
+    formRead.value = 'unread'
 }
 
 function submitBookForm(event) {
     event.preventDefault()   
-    addBook()
-    generateBookElement()
+    newBook().addFromForm()
     toggleNewBookModal()
     clearForm()
 }
@@ -91,11 +153,11 @@ function toggleBookRead(element) {
             library[i].toggleRead()
         }
     }
-    element.textContent = element.textContent === "not read" ? "read" : "not read"
+    element.textContent = element.textContent === 'unread' ? 'read' : 'unread'
 }
 
 function toggleInfoModal() {
-    infoModal.style.display = infoModal.style.display === "flex" ? "none" : "flex"
+    infoModal.style.display = infoModal.style.display === 'flex' ? 'none' : 'flex'
 }
 
 function displayBookInfo(element) {
